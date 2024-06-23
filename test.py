e076889f2fcd54caba644ddaa8caa9983ddd0106
PangYuanbo <yuanbopang@outlook.com>
2024-06-23 07:29:00 -0700

    init

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import locale
 locale.getpreferredencoding = lambda: "UTF-8"
+import numpy as np
+
+print(np.__version__)
+print(dir(np))  # 查看 numpy 模块中有哪些属性
+
 from transformers import AutoTokenizer, BitsAndBytesConfig
 from llava.model import LlavaLlamaForCausalLM
 import torch
@@ -13,7 +18,7 @@ kwargs = {"device_map": "auto"}
 kwargs['load_in_4bit'] = True
 kwargs['quantization_config'] = BitsAndBytesConfig(
     load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_compute_dtype=torch.float32,
     bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type='nf4'
 )
